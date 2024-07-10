e688d4ecb4b80d1eae89584dffdf7ea8751fb822
cyril <cyril.porez@laplateforme.io>
2024-07-11 01:43:06 +0200

    feat react router

---
+++ front-end/src/Pages/Auth/sign-in.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignIn(params) {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
---
+++ front-end/src/Pages/Auth/sign-up.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUp(params) {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
+}
