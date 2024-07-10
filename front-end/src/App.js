a19a4973c7e4e42699dbe940c9e1df40a5098584
cyril <cyril.porez@laplateforme.io>
2024-07-11 01:43:06 +0200

    feat react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SignIn from "./Pages/Auth/sign-in";
+import SignUp from "./Pages/Auth/sign-up";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
