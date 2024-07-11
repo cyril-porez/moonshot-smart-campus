951cfaac5e147148d523b32de486d3f2af27a07a
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2024-07-11 11:39:00 +0200

    feat: header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignIn from "./Pages/Auth/sign-in";
 import SignUp from "./Pages/Auth/sign-up";
+import Home from "./Pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+    <Header logo={"/logo.png"} currentUser={"Ibrahim"}/>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
