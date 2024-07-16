af3acbfade5b41a6646bcb7169485eb9d1609ee2
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-07-16 14:24:07 +0200

    update database

---
+++ back-end/src/api/cursus/controllers/cursus.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cursus controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cursus.cursus');
