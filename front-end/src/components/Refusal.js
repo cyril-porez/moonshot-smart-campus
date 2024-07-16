4fe17500488bdf725591b170680cf229d00461d8
lucas-ribard <lucas.ribard@laplateforme.io>
2024-07-16 16:06:31 +0200

    Added : Refusal Modal

---
+++ front-end/src/components/Refusal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import "../style/Refusal.css";
+
+export default function Refusal({ Promo, Subject}) {
+
+    return (
+        <div >
+           <form className="RefusalBody"> 
+            <h2>Motif de Refus</h2>
+            <h3>Promo : {Promo} </h3>
+            <h3>Sujet : {Subject} </h3>
+
+            <textarea name="Refusal" id="Refusal" cols="30" rows="10" placeholder="Motif de refus"></textarea>
+
+            <input className="ButtonRefusal" type="submit" value="Valider"/>
+           </form>
+
+
+        </div>
+    );
+}
