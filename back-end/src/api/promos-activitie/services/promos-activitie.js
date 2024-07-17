ea1ec5754d0655121f242f0ae9880ea1e0887039
cyril <cyril.porez@laplateforme.io>
2024-07-17 20:29:42 +0200

    add attribut and relation in table bdd

---
+++ back-end/src/api/promos-activitie/services/promos-activitie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * promos-activitie service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::promos-activitie.promos-activitie');
