89cb601c1ffa03e9255a05e951451dfcf09d0518
Thibault Kine <thibault.kine@laplateforme.io>
2024-07-22 16:46:43 +0200

    fix: renamed Modal.js and added missing brackeys

--- front-end/src/components/Tables.js
+++ front-end/src/components/Tables.js
@@ -24,88 +24,89 @@ export function ActivityTable({ data = [], type }) {
     const closeModal = () =>
         setModalState({ isOpen: false, type: null, activity: null });
 
-  const handleRefuseActivity = (activity) => openModal("refusal", activity);
-  const handleValidateActivity = (activity) =>
-    openModal("newActivity", activity);
-  const handleSuiviActivity = (activity) => openModal("suivi", activity);
-  const handleNotesActivity = (activity) => openModal("notes", activity);
+    const handleRefuseActivity = (activity) => openModal("refusal", activity);
+    const handleValidateActivity = (activity) =>
+        openModal("newActivity", activity);
+    const handleSuiviActivity = (activity) => openModal("suivi", activity);
+    const handleNotesActivity = (activity) => openModal("notes", activity);
 
-  // Staff opinion of the activity
-  function evaluateActivity(id) {
-    navigate("/EvaluateActivity?id=" + id);
-  }
-  // Student opinion of the activity
-  function rateActivity(id) {
-    navigate("/ActivityReview?id=" + id);
-  }
-
-  const renderModalContent = () => {
-    const { type, activity } = modalState;
-    switch (type) {
-      case "refusal":
-        return <Refusal closeModal={closeModal} data={activity} />;
-      case "newActivity":
-        return <NewActivity closeModal={closeModal} data={activity} />;
-      case "suivi":
-        return <LaunchActivity closeModal={closeModal} data={activity} />;
-      case "notes":
-        return <Remarks closeModal={closeModal} data={activity} />;
-      default:
-        return null;
+    // Staff opinion of the activity
+    function evaluateActivity(id) {
+        navigate("/EvaluateActivity?id=" + id);
+    }
+    // Student opinion of the activity
+    function rateActivity(id) {
+        navigate("/ActivityReview?id=" + id);
     }
 
-  return (
-    <>
-      <table>
-        <thead>
-          <tr className="line">
-            <th>Sujet</th>
-            <th>Promo</th>
-            <th>Date</th>
-            <th>Heure</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="table-body">
-          {data.map((activity) => (
-            <tr className="line" key={activity.id}>
-              <td>{activity.subject}</td>
-              <td>{activity.promo}</td>
-              <td>{activity.date}</td>
-              <td>{activity.time}</td>
-              <td>
-                {type === "suivi" ? (
-                  <FormButton
-                    text={"Lancer"}
-                    onClick={() => handleSuiviActivity(activity)}
-                  />
-                ) : type === "proposition" ? (
-                  <>
-                    <FormButton
-                      text={"Valider"}
-                      onClick={() => handleValidateActivity(activity)}
-                    />
-                    <FormButton
-                      text={"Refuser"}
-                      onClick={() => handleRefuseActivity(activity)}
-                    />
-                  </>
-                ) : type === "evaluer" ? (
-                  <FormButton
-                    text={"Evaluer"}
-                    onClick={() => evaluateActivity()}
-                  />
-                ) : type === "notes" ? (
-                  <FormButton
-                    text={"Voir notes"}
-                    onClick={() => handleNotesActivity(activity)}
-                  />
-                ) : null}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    const renderModalContent = () => {
+        const { type, activity } = modalState;
+        switch (type) {
+            case "refusal":
+                return <Refusal closeModal={closeModal} data={activity} />;
+            case "newActivity":
+                return <NewActivity closeModal={closeModal} data={activity} />;
+            case "suivi":
+                return <LaunchActivity closeModal={closeModal} data={activity} />;
+            case "notes":
+                return <Remarks closeModal={closeModal} data={activity} />;
+            default:
+                return null;
+        }
+    }
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr className="line">
+                        <th>Sujet</th>
+                        <th>Promo</th>
+                        <th>Date</th>
+                        <th>Heure</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody className="table-body">
+                    {data.map((activity) => (
+                        <tr className="line" key={activity.id}>
+                            <td>{activity.subject}</td>
+                            <td>{activity.promo}</td>
+                            <td>{activity.date}</td>
+                            <td>{activity.time}</td>
+                            <td>
+                                {type === "suivi" ? (
+                                    <FormButton
+                                        text={"Lancer"}
+                                        onClick={() => handleSuiviActivity(activity)}
+                                    />
+                                ) : type === "proposition" ? (
+                                    <>
+                                        <FormButton
+                                            text={"Valider"}
+                                            onClick={() => handleValidateActivity(activity)}
+                                        />
+                                        <FormButton
+                                            text={"Refuser"}
+                                            onClick={() => handleRefuseActivity(activity)}
+                                        />
+                                    </>
+                                ) : type === "evaluer" ? (
+                                    <FormButton
+                                        text={"Evaluer"}
+                                        onClick={() => evaluateActivity()}
+                                    />
+                                ) : type === "notes" ? (
+                                    <FormButton
+                                        text={"Voir notes"}
+                                        onClick={() => handleNotesActivity(activity)}
+                                    />
+                                ) : null}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
             {modalState.isOpen && (
                 <Modal isOpen={modalState.isOpen} onClose={closeModal}>
