7cb5afb3cec6c79438563d76402b34f3e6c46b74
lucas-ribard <lucas.ribard@laplateforme.io>
2024-07-22 16:07:05 +0200

    Fix : wrong toggle state for button

--- front-end/src/Pages/Staff/ActivityNotes.js
+++ front-end/src/Pages/Staff/ActivityNotes.js
@@ -16,31 +16,13 @@ export function ActivityNotes(userType) {
     //and use them as filter to get those
     const dataSet1 = [{ id: 1, subject: "I want to kill myself", promo: "B2", activityTypes: "HowTo", helper: "Habib", date: "2024-07-18", time: "09h00", expectedTime: 30, room: "A2", remark: "C'était claqué au sol" }]
     const dataSet2 = [{ id: 1, subject: "I want to kill myself faster", promo: "B2", activityTypes: "HowTo", helper: "Habib", date: "2024-07-18", time: "09h00", expectedTime: 30, room: "A2", remark: "HABIIIIIIIIIIIB" }]
-    
+
 
     return (
         <div>
             {userType.userType === "responsable" ? (
                 <>
                     {isToggled ? (
-                        <>
-                            <h1>Notes des élèves</h1>
-                            <select>
-                                <option hidden value="">Promo</option>
-                                {promo.map((option, index) => (
-                                    <option key={index} value={option}>
-                                        
-                                        {option}
-                                    </option>
-                                ))}
-                            </select>
-                            <button onClick={handleToggle}>
-                                {isToggled ? 'Notes élèves' : 'Notes accompagnateurs'}
-                            </button>
-                          
-                            <ActivityTable type={"notes"} data={dataSet1} />
-                        </>
-                    ) : (
                         <>
                             <h1>Notes des accompagnateurs</h1>
                             <select>
@@ -64,6 +46,26 @@ export function ActivityNotes(userType) {
                             </button>
                             <ActivityTable type={"notes"} data={dataSet2} />
                         </>
+                    ) : (
+
+
+                        <>
+                            <h1>Notes des élèves</h1>
+                            <select>
+                                <option hidden value="">Promo</option>
+                                {promo.map((option, index) => (
+                                    <option key={index} value={option}>
+
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                            <button onClick={handleToggle}>
+                                {isToggled ? 'Notes élèves' : 'Notes accompagnateurs'}
+                            </button>
+
+                            <ActivityTable type={"notes"} data={dataSet1} />
+                        </>
 
                     )}
 
@@ -74,13 +76,13 @@ export function ActivityNotes(userType) {
                     <>
                         <h1>Notes des élèves</h1>
                         <select>
-                                <option hidden value="">Promo</option>
-                                {promo.map((option, index) => (
-                                    <option key={index} value={option}>
-                                        {option}
-                                    </option>
-                                ))}
-                            </select>
+                            <option hidden value="">Promo</option>
+                            {promo.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
                         <ActivityTable type={"note"} data={dataSet1} />
                     </>
                 ) : null}
