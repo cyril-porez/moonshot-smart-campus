8bb6367bcc0e4f0a4d598d0ca7130aa8644d3a7c
cyril <cyril.porez@laplateforme.io>
2024-07-24 11:57:54 +0200

    table propsition

---
+++ back-end/src/api/proposition/controllers/proposition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * proposition controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::proposition.proposition');
---
+++ back-end/src/api/proposition/routes/proposition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * proposition router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::proposition.proposition');
